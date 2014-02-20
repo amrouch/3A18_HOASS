bb202c92ff215e5fae75dcbe44d74fc2989ed78b
omar <omar@omar-PC>
2014-02-20 15:21:58 +0100

    Création Projet

---
+++ tuniPharma/src/tunipharma/Admin.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tunipharma;
+
+/**
+ *
+ * @author omar
+ */
+public class Admin {
+    private int idAdmin;
+    
+}
